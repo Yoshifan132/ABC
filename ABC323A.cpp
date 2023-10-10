f5be2dcc4643c3f6276a756da0b5089bc7a7bb7e
Yoshifan132 <147536544+Yoshifan132@users.noreply.github.com>
2023-10-11 01:13:57 +0900

    Add files via upload

---
+++ ABC323A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define mod 998244353
+#define inv2 499122177
+using namespace std;
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+int main() {
+    string s;
+    cin>>s;
+    for(int i=0;i<s.size();i++){
+        if(i%2==1){
+            if(s[i]!='0'){
+                cout<<"No"<<endl;
+                return 0;
+            }
+        }
+    }
+    cout<<"Yes"<<endl;
+}
