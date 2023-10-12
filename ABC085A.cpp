b8f2aaa0efa80070d147ce24f3604798446235a7
Ryohei Ishii <147536544+Yoshifan132@users.noreply.github.com>
2023-10-12 17:00:40 +0900

    Add files via upload

---
+++ ABC085A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define mod 998244353
+#define inv2 499122177
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for(ll i=0;i<ll(n);i++)
+
+int main() {
+    string s;
+    cin>>s;
+    s[3]='8';
+    cout<<s<<endl;
+    return 0;
+}
