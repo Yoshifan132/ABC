c6fea81cdb1cd01706de14a47887ec63cb660bf8
Ryohei Ishii <147536544+Yoshifan132@users.noreply.github.com>
2023-10-16 18:41:08 +0900

    Add files via upload

---
+++ 317B.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define mod 998244353
+#define inv2 499122177
+using namespace std;
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+int main() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    rep(i,0,n) cin>>a[i];
+    sort(a.begin(),a.end());
+    rep(i,0,n-1){
+        if(a[i+1]!=a[i]+1){
+            cout<<a[i]+1<<endl;
+            return 0;
+        }
+    }
+}
