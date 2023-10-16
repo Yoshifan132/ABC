b4797f19c4954f0815c191bdd909a333305c9d6a
Ryohei Ishii <147536544+Yoshifan132@users.noreply.github.com>
2023-10-16 19:04:19 +0900

    Add files via upload

---
+++ 242B.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define mod 998244353
+#define inv2 499122177
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for(ll i=0;i<ll(n);i++)
+
+int main() {
+    string s;
+    cin>>s;
+    sort(s.begin(),s.end());
+    cout<<s<<endl;
+}
